b6df6d8334e0ede3cacbb85f97685370166350df
Tomas Yaya <tomasyaya@ip-192-168-0-13.eu-west-1.compute.internal>
2021-03-02 10:18:11 -0300

    fix cors config

--- configs/middleware.config.js
+++ configs/middleware.config.js
@@ -2,10 +2,10 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const origin = "http://localhost:3000/";
+const origin = "http://localhost:3000";
 
 module.exports = (app) => {
-  app.use(cors({ origin: [origin] }));
+  app.use(cors({ origin: [origin], credentials: true }));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(cookieParser());
